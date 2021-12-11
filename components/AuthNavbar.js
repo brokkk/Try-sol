289f43fe744f3e6445c0c3b5648de021286e6b52
brokkk <tiontravolta@gmail.com>
2021-12-12 06:57:47 +0700

    okray

---
+++ components/AuthNavbar.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Link from "next/link";
+// components
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import PagesDropdown from "../components/PagesDropdown.js";
+
+export default function Navbar(props) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  return (
+    <>
+      <nav className="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-black shadow">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <Link href="/">
+              <a
+                className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+                href="#pablo"
+              >
+                RobotS NFTs
+              </a>
+            </Link>
+            <button
+              className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <i className="text-white fas fa-bars"></i>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none" +
+              (navbarOpen ? " block rounded shadow-lg" : " hidden")
+            }
+            id="example-navbar-warning"
+          >
+
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+
+              <li className="flex items-center">
+                <a
+                  className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  href="#"
+                  target="_blank"
+                >
+                  <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-facebook text-lg leading-lg " />
+                  <span className="lg:hidden inline-block ml-2">Share</span>
+                </a>
+              </li>
+
+              <li className="flex items-center mr-3">
+                <a
+                  className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                  href="#"
+                  target="_blank"
+                >
+                  <i className="lg:text-blueGray-200 text-blueGray-400 fab fa-twitter text-lg leading-lg " />
+                  <span className="lg:hidden inline-block ml-2">Tweet</span>
+                </a>
+              </li>
+
+              <li className="flex items-center">
+                <WalletMultiButton />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
