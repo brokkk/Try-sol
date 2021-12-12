8344c6ec32ea5be7462b122fb8238c42b5e85742
brokkk <tiontravolta@gmail.com>
2021-12-12 07:12:21 +0700

    oks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import Image from 'next/image'
 
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import useCandyMachine from "../hooks/useCandyMachine";
@@ -165,7 +166,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <img
+        <Image
           className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860-px"
           src="https://demos.creative-tim.com/notus-nextjs/img/pattern_nextjs.png"
           alt="..."
@@ -179,8 +180,8 @@ export default function Home() {
           <div className="container mx-auto px-4">
             <div className="items-center flex flex-wrap">
               <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
-                <img
-                  alt="..."
+                <Image
+                  alt="ok"
                   className="max-w-full rounded-lg shadow-lg"
                   src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
                 />
@@ -194,7 +195,7 @@ export default function Home() {
                   <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
                     The extension comes with three pre-built pages to help you
                     get started faster. You can change the text and images and
-                    you're good to go.
+                    you&apos;re good to go.
                   </p>
                   <ul className="list-none mt-6">
                     <li className="py-2">
